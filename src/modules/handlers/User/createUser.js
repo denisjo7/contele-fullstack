ad550c933d13fc251b86bea16391e45de61778e3
Denis Jonathan <denisjonathan7@gmail.com>
2023-04-04 11:06:51 -0300

    fix: assincronicidade ao utilizar o método createUserService

--- src/modules/handlers/User/createUser.js
+++ src/modules/handlers/User/createUser.js
@@ -3,26 +3,26 @@ const httpStatusCodes = require('http-status-codes');
 const { httpErrorHandler } = require("../../common/handlers");
 const { createUserService } = require('../../services');
 
-const createUserHandler = (req, res, next) => {
-    try{
-        const {
-            user_email,
-            user_password,
-            full_name
-        } = req.body
+const createUserHandler = async (req, res, next) => {
+  try {
+    const {
+      user_email,
+      user_password,
+      full_name
+    } = req.body
 
-        const created_user = createUserService({
-            user_email,
-            user_password,
-            full_name
-        })
+    const created_user = await createUserService({
+      user_email,
+      user_password,
+      full_name
+    })
 
-        return res.status(httpStatusCodes.OK).send(created_user);
-    }catch(error){
-        return httpErrorHandler({ req, res, error })
-    }
+    return res.status(httpStatusCodes.OK).send(created_user);
+  } catch (error) {
+    return httpErrorHandler({ req, res, error })
+  }
 }
 
 module.exports = {
-    createUserHandler
+  createUserHandler
 }

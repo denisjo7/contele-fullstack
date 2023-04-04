4e213f0e7920d92e5f9b393c5b87721076953570
Denis Jonathan <denisjonathan7@gmail.com>
2023-04-04 13:08:47 -0300

    fix: especifica qual post deve ser deletado

--- src/modules/repositories/Post/deletePostRepositories/deletePostRepositories.js
+++ src/modules/repositories/Post/deletePostRepositories/deletePostRepositories.js
@@ -1,25 +1,23 @@
 const {
-    getTransaction,
-    commitTransaction,
-    rollbackTransaction
+  getTransaction,
+  commitTransaction,
+  rollbackTransaction
 } = require('../../../common/handlers')
 
-const deletePostRepositories = async (
-    post_id
-) => {
-    const { transaction } = await getTransaction();
+const deletePostRepositories = async ({ post_id }) => {
+  const { transaction } = await getTransaction();
 
-    try {
-        await transaction('posts').del()
+  try {
+    await transaction('posts').where({ id: post_id }).del()
 
-        await commitTransaction({transaction})
-        
-    } catch (err) {
-        rollbackTransaction({transaction})
-        throw new Error(err)
-    }
+    await commitTransaction({ transaction })
+
+  } catch (err) {
+    rollbackTransaction({ transaction })
+    throw new Error(err)
+  }
 }
 
 module.exports = {
-    deletePostRepositories
+  deletePostRepositories
 }

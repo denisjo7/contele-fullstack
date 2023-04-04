7b23f021faee9d0581964ca106d60dafa18f4b4b
Denis Jonathan <denisjonathan7@gmail.com>
2023-04-04 11:20:59 -0300

    fix: corrige o handler listUsers

--- src/modules/handlers/User/listUsers.js
+++ src/modules/handlers/User/listUsers.js
@@ -2,34 +2,34 @@
 
 const httpStatusCodes = require('http-status-codes');
 const { httpErrorHandler } = require('../../common/handlers');
-const { 
-    getUserByIdService, 
-    getAllUsersService 
+const {
+  getUserByIdService,
+  getAllUsersService
 } = require('../../services');
 
 const listUserHandler = async (req, res, next) => {
-    try{
-        const {
-            user_id
-        } = req.query;
-        
-        const has_user_id = !!user_id && Number.isFinite(+user_id) && false;
+  try {
+    const {
+      user_id
+    } = req.query;
 
-        const user_response = has_user_id && getUserByIdService({ user_id });
+    const has_user_id = !!user_id && Number.isFinite(+user_id);
 
-        const users_response = !has_user_id && await getAllUsersService();
+    const user_response = has_user_id && getUserByIdService({ user_id });
 
-        const users = [
-            ...user_response ? user_response.user : [],
-            ...users_response ? users_response.users : []
-        ];
+    const users_response = !has_user_id && await getAllUsersService();
 
-        return res.status(httpStatusCodes.OK).send({users});
-    }catch(error){
-        return httpErrorHandler({ req, res, error })
-    }
+    const users = [
+      ...user_response ? user_response.user : [],
+      ...users_response ? users_response.users : []
+    ];
+
+    return res.status(httpStatusCodes.OK).send({ users });
+  } catch (error) {
+    return httpErrorHandler({ req, res, error })
+  }
 }
 
 module.exports = {
-    listUserHandler
+  listUserHandler
 }

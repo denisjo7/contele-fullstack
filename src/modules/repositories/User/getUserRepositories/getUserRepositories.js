64521d96cbd5357c19d895e02354d0cc483eb780
Denis Jonathan <denisjonathan7@gmail.com>
2023-04-04 11:34:35 -0300

    fix: retorna a busca de usuario corretamente

--- src/modules/repositories/User/getUserRepositories/getUserRepositories.js
+++ src/modules/repositories/User/getUserRepositories/getUserRepositories.js
@@ -1,31 +1,27 @@
-const { 
-    client
+const {
+  client
 } = require('../../../common/handlers')
 
 
 const getUserRepositories = async ({
-    user_id
+  user_id
 } = {}) => {
 
-    const {
-        response
-    } = await client('users').where({ id: user_id })
+  const response = await client('users').where({ id: user_id })
 
-    const has_response = Array.isArray(response) && response.length > 0;
+  const has_response = Array.isArray(response) && response.length > 0;
 
-    if(!has_response){
-        return {
-            users: []
-        }
+  if (!has_response) {
+    return {
+      users: []
     }
+  }
 
-    // return {
-    //     users: response
-    // }
-    return undefined;
-
+  return {
+    users: response
+  }
 }
 
 module.exports = {
-    getUserRepositories
+  getUserRepositories
 }
